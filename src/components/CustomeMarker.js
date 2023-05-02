4f80ecf1e0427e92d1b80c66325fd73101bfccb5
unknown <AvinashPandey2000>
2023-05-02 05:36:02 +0530

    map :add the custome marke for location

---
+++ src/components/CustomeMarker.js
@@ -0,0 +1,26 @@
+import {View, Text, StyleSheet,Image} from 'react-native';
+import React from 'react';
+
+export default function CustomeMarker({src}) {
+    console.log('src :',src)
+  return (
+    <View style={styles.conariner}>
+    <Image source={{uri: src}}
+    style={styles.image} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  conariner: {
+    height: 40,
+    width: 40,
+    borderRadius: 50,
+    // backgroundColor: '#FFFF',
+  },
+  image: {
+    height: 40,
+    width: 40,
+    borderRadius: 50,
+  },
+});
